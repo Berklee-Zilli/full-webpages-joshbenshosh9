5dfd4ad261627b7a162b33c5a2f5f44fb606fa95
Joshua Ben-Shoshan <joshuaben-shoshan@Joshuas-MacBook-Pro-3.local>
2024-11-19 19:07:38 -0500

    Level 1 Complete

---
+++ task1/using-js-to-make-changes.js
@@ -0,0 +1,4 @@
+document.title = "Task 1 by Joshua Ben-Shoshan";
+document.querySelector("h1").textContent = "TASK 1";
+document.getElementById("intro").textContent = "I love baseball way too much.";
+document.querySelector("h2").textContent = "List of the things I like";
